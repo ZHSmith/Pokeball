9536ecd889eb9db70d388f13f5a272a499ce891e
ZHSmith <43040663+ZHSmith@users.noreply.github.com>
2018-09-11 07:57:25 -0700

    AddedPokeFile

---
+++ pokeball.java
@@ -0,0 +1,20 @@
+
+public class pokeball {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("   @@@@@@@@@@\r\n" + 
+				           "  @@@@@@@@@@@@\r\n" + 
+				           " @@@@@@@@@@@@@@\r\n" + 
+				           "@@@@@@@@@@@@@@@@\r\n" + 
+				           "|||||[““““]|||||\r\n" + 
+				           "|||||[....]|||||\r\n" + 
+				           "OOOOOOOOOOOOOOOO\r\n" + 
+				           " OOOOOOOOOOOOOO\r\n" + 
+				           "  OOOOOOOOOOOO\r\n" + 
+				           "   OOOOOOOOOO\r\n");
+		
+	}
+
+}
